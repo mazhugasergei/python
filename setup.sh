978554bb0e3df0d03b51369bcbee8feea5116d5c
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2025-10-27 22:20:14 +1000

    add cols plot

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-if [ ! -d ".venv" ]; then
-  echo "Creating virtual environment..."
-  python3 -m venv .venv
-fi
-
+rm -rf .venv
+python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
